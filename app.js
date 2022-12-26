4e76983eb257d267bc5d1d8125274fcfc6a1a372
roliestpoliest <carolynheron1@gmail.com>
2022-12-26 04:27:49 -0600

    began implantation on main content

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+'use strict';
+
 // selectors
-const selectGradient = document.getElementById("select-gradient");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 
 // event listeners
-// selectGradient.addEventListener("click", gradientSelector);
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
 // functions
 function setGradient(event){
     console.log("gradient selector function");
-    const body = document.getElementById("use-gradient");
-    // body.style.backgroundColor = "#a0c4ff"; 
-    body.style.background = "linear-gradient(120deg, " + color1.value + ", " + color2.value + ")";
+    const content = document.getElementById("use-gradient");
+    // content.style.backgroundColor = "#a0c4ff"; 
+    content.style.background = "linear-gradient(180deg, " + color1.value + ", " + color2.value + ")";
+    document.body.style.backgroundColor = color2.value;
 }
 
